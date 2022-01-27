b7c997e4bec1bea23a8a68e7e8ddf5b396b1151c
missaelcv <missaelcv0220@gmail.com>
2022-01-26 23:11:15 -0400

    Style 2 form

--- index.php
+++ index.php
@@ -5,9 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome To youe App</title>
-   
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
-    <link real="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <h1>Please login or SignUp</h1>
---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+   
+</head>
+<body>
+    <h1>Login</h1>
+
+    <form action="login.php" method="post">
+    <input type="text" name="email" placeholder="Enter your Mail">
+    <input type="password" name="Password" placeholder="Enter Your Passoword">
+    <input type="submit" value="Send">
+</body>
+</html>
