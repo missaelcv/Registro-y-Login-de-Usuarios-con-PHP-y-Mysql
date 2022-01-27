e42857c3ed06ad2de4437cecd7d6c6ca7dea57a8
missaelcv <missaelcv0220@gmail.com>
2022-01-26 23:34:49 -0400

    Sigunp.php

---
+++ signup.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SignUp</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<?php require 'partials/header.php' ?>
+
+<h2>Hola </h2>
+    
+</body>
+</html>
