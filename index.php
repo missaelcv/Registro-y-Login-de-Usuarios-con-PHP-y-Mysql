c42e48a2d0338d0bef32d9419c7f85c5a9baee22
missaelcv <missaelcv0220@gmail.com>
2022-01-26 22:44:13 -0400

    Creando Style

--- index.php
+++ index.php
@@ -5,12 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome To youe App</title>
+   
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <link real="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <h1>Please login or SignUp</h1>
 
     <a href="login.php">Login</a> or
-    <a href="signug.php">SignUp</a> or
-
+    <a href="signug.php">SignUp</a> 
 </body>
 </html>
