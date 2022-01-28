6baefa7d7ac6d386980f804dbcc02a82deb33ab4
missaelcv <missaelcv0220@gmail.com>
2022-01-27 23:33:20 -0400

    validado la conexion de sigunp con el locahost

--- signup.php
+++ signup.php
@@ -1,27 +1,48 @@
 <?php
 
+  require 'database.php';
+
+  $message = '';
+
+  if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':email', $_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password);
+
+    if ($stmt->execute()) {
+      $message = 'Successfully created new user';
+    } else {
+      $message = 'Sorry there must have been an issue creating your account';
+    }
+  }
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
+<html>
+  <head>
+    <meta charset="utf-8">
     <title>SignUp</title>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
+  </head>
+  <body>
 
-<?php require 'partials/header.php' ?>
+    <?php require 'partials/header.php' ?>
 
-<h2>SignUp </h2>
-<span> or <a href="login.php">Login</a></span>
-<form action="login.php" method="post">
-    <input type="text" name="email" placeholder="Enter your Mail">
-    <input type="password" name="password" placeholder="Enter Your Passoword">
-    <input type="password" name="confirm_password" placeholder="Enter Your Passoword">
-    <input type="submit" value="send">
+    <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
+
+    <h1>SignUp</h1>
+    <span>or <a href="login.php">Login</a></span>
+
+    <form action="signup.php" method="POST">
+      <input name="email" type="text" placeholder="Enter your email">
+      <input name="password" type="password" placeholder="Enter your Password">
+      <input name="confirm_password" type="password" placeholder="Confirm Password">
+      <input type="submit" value="Submit">
     </form>
 
-    
-</body>
+  </body>
 </html>
